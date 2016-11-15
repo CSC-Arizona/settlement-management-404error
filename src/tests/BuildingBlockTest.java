b7a9b746b6e9280e4f92f332d81c4a4f7bd173ec
Katherine <katieew@email.arizona.edu>
2016-11-15 16:54:57 -0700

    Added some more test cases

--- src/tests/BuildingBlockTest.java
+++ src/tests/BuildingBlockTest.java
@@ -2,19 +2,25 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.awt.Color;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.Test;
 
+import model.Map;
 import model.BuildingBlocks.AirBlock;
 import model.BuildingBlocks.AntTunnelBlock;
+import model.BuildingBlocks.BushBlock;
 import model.BuildingBlocks.CavernBlock;
 import model.BuildingBlocks.EarthBlock;
+import model.BuildingBlocks.GoldOreBlock;
 import model.BuildingBlocks.IronOreBlock;
 import model.BuildingBlocks.LavaBlock;
+import model.BuildingBlocks.LeafBlock;
 import model.BuildingBlocks.StoneBlock;
 import model.BuildingBlocks.TreeRootBlock;
+import model.BuildingBlocks.WoodBlock;
 import model.Items.AntLarvaItem;
 import model.Items.IronItem;
 import model.Items.Item;
@@ -28,6 +34,51 @@ import model.Items.WoodItem;
  */
 public class BuildingBlockTest {
 
+	@Test
+	public void tryingToGetSomeMapCodeCoverage() {
+		Map m = new Map(100, 100, 0, 0);
+	}
+	
+	@Test
+	public void testLeafBLock() {
+		LeafBlock lb = new LeafBlock();
+		assertFalse(lb.isOccupiable());
+		assertTrue(lb.isDestroyable());
+		assertEquals(new LinkedList(), lb.lootBlock());
+		assertEquals(1, lb.getDurability());
+		assertEquals(new Color(84, 232, 67), lb.getColor());
+	}
+	
+	@Test
+	public void testBushBlock() {
+		BushBlock bb = new BushBlock();
+		assertFalse(bb.isOccupiable());
+		assertTrue(bb.isDestroyable());
+		assertEquals(new LinkedList(), bb.lootBlock());
+		assertEquals(1, bb.getDurability());
+		assertEquals(new Color(0, 87, 3), bb.getColor());
+	}
+	
+	@Test
+	public void testGoldOreBlock() {
+		GoldOreBlock gob = new GoldOreBlock();
+		assertFalse(gob.isOccupiable());
+		assertTrue(gob.isDestroyable());
+		assertEquals(new LinkedList(), gob.lootBlock());
+		assertEquals(10, gob.getDurability());
+		assertEquals(new Color(255, 223, 0), gob.getColor());
+	}
+	
+	@Test
+	public void testWoodBlock() {
+		WoodBlock wb = new WoodBlock();
+		assertFalse(wb.isOccupiable());
+		assertTrue(wb.isDestroyable());
+		assertEquals(new WoodItem().getClass(), wb.lootBlock().get(0).getClass());
+		assertEquals(5, wb.getDurability());
+		assertEquals(new Color(174, 144, 55), wb.getColor());
+	}
+	
 	@Test
 	public void testAirBlock() {
 		AirBlock ab = new AirBlock();
