592558ec0dd81a9f05c07710e2926e143cd7812f
Jonathon Davis <jonathondevindavis@gmail.com>
2016-11-15 15:50:59 -0700

    updated tests

--- src/tests/AllTests.java
+++ src/tests/AllTests.java
@@ -12,7 +12,8 @@ import org.junit.runners.Suite.SuiteClasses;
  *
  */
 @RunWith(Suite.class)
-@SuiteClasses({ AttackActionTest.class, MoveActionTest.class, BuildingBlockTest.class, InventoryTest.class, ItemTest.class })
+@SuiteClasses({ ArmorTest.class, AttackActionTest.class, BuildingBlockTest.class, InventoryTest.class, ItemTest.class,
+		MoveActionTest.class })
 public class AllTests {
 
 }
