98721d4fd4e1919f47654f716a8a10e4386a9601
Maxwell Faridian <maxwellfaridian@email.arizona.edu>
2016-11-22 16:40:35 -0700

    Updated Craft Action

--- src/model/Actors/CraftAction.java
+++ src/model/Actors/CraftAction.java
@@ -1,7 +1,9 @@
 package model.Actors;
 
+import java.util.Iterator;
 import java.util.List;
 
+import model.Inventory;
 import model.Items.Item;
 
 //Author: Maxwell Faridian
@@ -19,15 +21,35 @@ public class CraftAction implements Action {
 		this.requiredMaterials = requiredMaterials;
 	}
 
-	//Could check if actor has all required resources
 	//If actor doesn't have resources, send him to store room to get them
 	@Override
 	public int execute(Actor performer) {
 		// TODO Auto-generated method stub
 		//Find actor's inventory
-		//For loop through required materials, make sure actor has all in inventory
+		//For loop through required materials, and remove each item from performer's inventory
 		//Then, use up those items and literally create a new object of the craftableItem that they wanted, and add that 
 		//to the actor's inventory
+		
+		//For now, let's assume that actor has all necessary items
+		Inventory performerInventory = performer.getInventory();
+		//Go through list of requiredMaterials and for each one, remove that item from performer's inventory
+		for (int i = 0; i < requiredMaterials.size(); i++) {
+			Iterator<Item> inventoryIterator = performerInventory.iterator();
+			int indexOfItem = 0;
+			int collected = 0;
+			do {
+				if(inventoryIterator.next().getClass() == requiredMaterials.get(i).getClass()) {
+					performerInventory.removeItem(indexOfItem);
+					collected = 1;
+					
+				}
+				indexOfItem++;
+			} while(collected == 0);
+		}
+		
+		//Add new crafted item to performer's inventory
+		performer.getInventory().addItem(toBeCrafted);
+		
 		return 0;
 	}
 
