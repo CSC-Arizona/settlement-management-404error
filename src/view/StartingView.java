2de521a859ebe0f554a1bf868605cc5d21cc2511
Jonathon Davis <jonathondevindavis@gmail.com>
2016-12-06 15:53:48 -0700

    Added menu outline

--- src/view/StartingView.java
+++ src/view/StartingView.java
@@ -5,16 +5,21 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
+import javax.imageio.ImageIO;
 import javax.swing.Box;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -53,9 +58,14 @@ public class StartingView extends JPanel {
 	private int visibleCornerX;
 	private int blockSizeY = (windowHeight) / visibleHeight;
 	private int blockSizeX = windowWidth / visibleWidth;
+	private Image menu;
 
 	public StartingView(Controller controller) {
 		this.controller = controller;
+		try {
+		    menu = ImageIO.read(new File("src/images/resources/scroll.png"));
+		} catch (IOException e) {
+		}
 		for (ImageEnum e : ImageEnum.values()) {
 			e.createBufferedImages(blockSizeY, blockSizeX);
 		}
@@ -214,7 +224,7 @@ public class StartingView extends JPanel {
 				drawTile(g2, row, col, i, j);
 			}
 		}
-
+		g2.drawImage(menu, windowWidth/5, windowHeight/15, windowWidth-windowWidth/3, windowHeight-windowHeight/6, null);
 	}
 
 	private class MyKeyListener implements KeyListener {
